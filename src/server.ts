da4b5a729255d881d96d377fc5b0f1364579ba6e
Matheus Pereira <matheus.tecinfor98@gmail.com>
2021-07-10 14:09:16 -0300

    feat: add and configuration eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,8 @@
-import express from 'express';
-import { UserController } from '@controllers/UserController';
+import express from 'express'
+import '@controllers/UsersController'
 
-const app = express();
+const app = express()
 
-app.get('/', (req, res) => {
-    return res.json({ message: 'Hello World' });
-});
+app.get('/', (request, response) => response.json({ message: 'Hello World' }))
 
-app.listen(3333);
+app.listen(3333)
